41b77fabcb3cc18bb87a4db0890c729bfedbb0c3
Purukitto <purukitto@gmail.com>
2019-08-10 17:31:22 +0530

    No thumb?

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,6 @@ import 'package:flox/songs.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:flox/components/NameNControls.dart';
-import 'package:flox/components/radial_seek.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -40,7 +39,7 @@ class _MyAppState extends State<MyApp> {
               child: new Container(
                 width: 150,
                 height: 150,
-                child: BuildSeek(
+                child: new RadialSeekBar(
                   child: new ClipOval(
                     clipper: new CircleClipper(),
                     child: new Image.network(
@@ -80,8 +79,6 @@ class CircleClipper extends CustomClipper<Rect> {
   }
 }
 
-/*
-
 class RadialSeekBar extends StatefulWidget {
   final double trackWidth;
   final Color trackColor;
@@ -94,16 +91,16 @@ class RadialSeekBar extends StatefulWidget {
   final Widget child;
 
   RadialSeekBar({
-    this.trackWidth =3,
+    this.trackWidth = 3,
     this.trackColor = Colors.grey,
-    this.progressWidth= 5,
-    this.progressColor=primaryBlack,
-    this.progressPercent=0,
-    this.thumbSize=10,
-    this.thumbColor=primaryBlack,
-    this.thumbPosition=0,
+    this.progressWidth = 5,
+    this.progressColor = primaryBlack,
+    this.progressPercent = 0,
+    this.thumbSize = 10,
+    this.thumbColor = primaryBlack,
+    this.thumbPosition = 0,
     this.child,
-  })
+  });
 
   @override
   _SeekBarState createState() => new _SeekBarState();
@@ -113,8 +110,64 @@ class _SeekBarState extends State<RadialSeekBar> {
   @override
   Widget build(BuildContext context) {
     return new CustomPaint(
-      painter: new RadialSeekBar(),
+      painter: new RadialSeekBarPainter(
+        trackWidth: widget.trackWidth,
+        trackColor: widget.trackColor,
+        progressWidth: widget.progressWidth,
+        progressColor: widget.progressColor,
+        progressPercent: widget.progressPercent,
+        thumbSize: widget.thumbSize,
+        thumbColor: widget.thumbColor,
+        thumbPosition: widget.thumbPosition,
+      ),
     );
   }
 }
-*/
+
+class RadialSeekBarPainter extends CustomPainter {
+  final double trackWidth;
+  final Paint trackPaint;
+  final double progressWidth;
+  final double progressPercent;
+  final Paint progressPaint;
+  final double thumbSize;
+  final Paint thumbPaint;
+  final double thumbPosition;
+
+  RadialSeekBarPainter({
+    @required this.trackWidth,
+    @required trackColor,
+    @required this.progressWidth,
+    @required progressColor,
+    @required this.progressPercent,
+    @required this.thumbSize,
+    @required thumbColor,
+    @required this.thumbPosition,
+  })  : trackPaint = new Paint()
+          ..color = trackColor
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = trackWidth,
+        progressPaint = new Paint()
+          ..color = progressColor
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = progressWidth
+          ..strokeCap = StrokeCap.round,
+        thumbPaint = new Paint()
+          ..color = thumbColor
+          ..style = PaintingStyle.fill;
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = new Offset(size.width / 2, size.height / 2);
+    final radius = min(size.height, size.width) / 2;
+    canvas.drawCircle(
+      center,
+      radius,
+      trackPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
